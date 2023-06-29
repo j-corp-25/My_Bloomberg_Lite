bced6b16c8360410f08726b1fff187a722513334
Jordy <jordycorporan95@gmail.com>
2023-06-29 10:11:03 -0400

    First real push

--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import _ from 'lodash';
+import Example from "./scripts/example"
+document.addEventListener("DOMContentLoaded", () => {
+    const main = document.getElementById("main")
+    new Example(main)
+})
 
-function component() {
-  const element = document.createElement('div');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-  return element;
-}
 
-document.addEventListener('DOMContentLoaded', function () {
-  document.body.appendChild(component());
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log("Hello world!!!")
 });
-
