8e0b3984589297307bbcc8e8935c5026f8d0c2e8
Jordy <jordycorporan95@gmail.com>
2023-06-29 20:04:41 -0400

    Added features to async function

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import Example from "./scripts/example"
 document.addEventListener("DOMContentLoaded", () => {
     const main = document.getElementById("main")
     new Example(main)
+
+
 })
 
 
--- src/scripts/example.js
+++ src/scripts/example.js
@@ -1,53 +1,58 @@
 class Example {
     constructor(ele) {
-        this.ele = ele
-        this.ele.innerHTML = "<h1>It's ALIVE!!!</h1>"
-        this.handleClick = this.handleClick.bind(this);
+      this.ele = ele;
+      this.ele.innerHTML = "<h1>It's ALIVE!!!</h1>";
+      this.handleClick = this.handleClick.bind(this);
+      this.ele.addEventListener("click", this.handleClick);
 
-
-        this.ele.addEventListener("click", this.handleClick);
-        console.log(this.getData());
-
+      this.getData();
     }
+
     handleClick() {
-        this.ele.children[0].innerText = "Ouch!"
+      this.ele.children[0].innerText = "Ouch!";
     }
 
     async getData() {
-    const url = 'https://bb-finance.p.rapidapi.com/market/get-price-chart?id=googl%3Aus&interval=y5';
-        const options = {
-            method: 'GET',
-            headers: {
-                'X-RapidAPI-Key': 'be2c5cf9d2mshdd2979e94314598p19c3dbjsnee3000224bd0',
-                'X-RapidAPI-Host': 'bb-finance.p.rapidapi.com'
-            }
-        };
-        let array = []
+      const url =
+        "https://apistocks.p.rapidapi.com/daily?symbol=AAPL&dateStart=2023-06-01&dateEnd=2023-06-29";
+      const options = {
+        method: "GET",
+        headers: {
+          "X-RapidAPI-Key": "be2c5cf9d2mshdd2979e94314598p19c3dbjsnee3000224bd0",
+          "X-RapidAPI-Host": "apistocks.p.rapidapi.com",
+        },
+      };
 
-        try {
-            const response = await fetch(url, options);
-            const result = await response.json();
-            // console.log(result);
-            array = array.concat(result)
-            console.log(array)
+      try {
+        const response = await fetch(url, options);
+        const result = await response.json();
 
-        } catch (error) {
-            console.error(error);
-        }
+        // Extracting ticker, closing prices, and dates
+        const ticker = this.getTicker(result);
+        const closingPrices = this.getClosingPrices(result);
+        const dates = this.getDates(result);
 
-
+        console.log("Ticker:", ticker);
+        console.log("Closing Prices:", closingPrices);
+        console.log("Dates:", dates);
+      } catch (error) {
+        console.error(error);
+      }
     }
 
+    getTicker(response) {
+      return response.Metadata.Symbol;
+    }
 
+    getClosingPrices(response) {
+      return response.Results.map((result) => result.Close);
+    }
 
-
-
-
-
-
-
-
-
+    getDates(response) {
+      return response.Results.map((result) => result.Date);
+    }
 }
+
+
 
 export default Example;
