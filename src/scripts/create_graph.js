eadbc09eb9f9affee8af0afded8cab9509e89bef
Jordy <jordycorporan95@gmail.com>
2023-06-30 00:49:23 -0400

    Fixed duplicate svg

--- src/scripts/create_graph.js
+++ src/scripts/create_graph.js
@@ -13,57 +13,66 @@ class CreateVisuals {
     }
 
 }
-    const stockData = [organizedData]
+    const dataset = organizedData
 
 
-    createChart(); {
-        const margin = {top: 20, right: 20, bottom:40, left:20 };
-        const width = 1600 - margin.left - margin.right;
-        const height = 800 - margin.top - margin.bottom;
-        const x = d3.scaletime()
-            .range([0, width]);
-        const y = d3.scale.linear()
-            .range([height, 0]);
-        const svg = d3.select(".chart-main")
-            .append("svg").attr("width", width + margin.left + margin.right)
-                          .attr("height", height + margin.top + margin.bottom)
-            .append("g").attr("transform", `translate(${margin.left},${margin.top})`);
+    // createChart(); {
+    //     const margin = {top: 20, right: 20, bottom:40, left:20 };
+    //     const width = 1600 - margin.left - margin.right;
+    //     const height = 800 - margin.top - margin.bottom;
+    //     const x = d3.scaletime()
+    //         .range([0, width]);
+    //     const y = d3.scale.linear()
+    //         .range([height, 0]);
+    //     const svg = d3.select(".chart-main")
+    //         .append("svg").attr("width", width + margin.left + margin.right)
+    //                       .attr("height", height + margin.top + margin.bottom)
+    //         .append("g").attr("transform", `translate(${margin.left},${margin.top})`);
+
+    //     x.domain(d3.extent(dataset,d => d.date));
+    //     y.domain([0, d3.max(dataset, d => d.value)]);
+
+
+        // svg.append("g")
+        //         .attr("transform", `translate
+
+
     }
 
 
 
 
-{
-    // Declare the chart dimensions and margins.
 
-    // Declare the x (horizontal position) scale.
-    const x = d3.scaleUtc()
-        .domain([new Date("2023-01-01"), new Date("2024-01-01")])
-        .range([marginLeft, width - marginRight]);
+//     // Declare the chart dimensions and margins.
 
-    // Declare the y (vertical position) scale.
-    const y = d3.scaleLinear()
-        .domain([0, 100])
-        .range([height - marginBottom, marginTop]);
+//     // Declare the x (horizontal position) scale.
+//     const x = d3.scaleUtc()
+//         .domain([new Date("2023-01-01"), new Date("2024-01-01")])
+//         .range([marginLeft, width - marginRight]);
 
-    // Create the SVG container.
-    const svg = d3.create("svg")
-        .attr("width", width)
-        .attr("height", height);
+//     // Declare the y (vertical position) scale.
+//     const y = d3.scaleLinear()
+//         .domain([0, 100])
+//         .range([height - marginBottom, marginTop]);
 
-    // Add the x-axis.
-    svg.append("g")
-        .attr("transform", `translate(0,${height - marginBottom})`)
-        .call(d3.axisBottom(x));
+//     // Create the SVG container.
+//     const svg = d3.create("svg")
+//         .attr("width", width)
+//         .attr("height", height);
 
-    // Add the y-axis.
-    svg.append("g")
-        .attr("transform", `translate(${marginLeft},0)`)
-        .call(d3.axisLeft(y));
+//     // Add the x-axis.
+//     svg.append("g")
+//         .attr("transform", `translate(0,${height - marginBottom})`)
+//         .call(d3.axisBottom(x));
 
-    // Return the SVG element.
-    return svg.node();
-  }
+//     // Add the y-axis.
+//     svg.append("g")
+//         .attr("transform", `translate(${marginLeft},0)`)
+//         .call(d3.axisLeft(y));
+
+//     // Return the SVG element.
+//     return svg.node();
+//   }
 
 
 export default create
