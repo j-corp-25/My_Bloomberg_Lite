107423ce3397cd766e365d13cbf9291e41424e17
Jordy <jordycorporan95@gmail.com>
2023-07-04 12:40:59 -0400

    Gave proper credits to d3.js devlopers

--- src/scripts/createChart.js
+++ src/scripts/createChart.js
@@ -1,4 +1,5 @@
-//credit to d3.js developers for the template below which I used to create and populate the chart https://d3js.org/getting-started#try-d3-online
+//credit to d3.js developers for the template below which I used to create and populate my chart
+//https://d3js.org/getting-started#try-d3-online
 function createChart(){
     //Define the chart date format
     let dateFormat = d3.timeFormat("%Y-%M-%D");
