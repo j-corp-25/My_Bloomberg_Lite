9c173de3ca6a3a6c2ab40a1ff51429217d0d5613
Jordy <jordycorporan95@gmail.com>
2023-07-05 04:06:59 -0400

    Added consts for ticker and buttons

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import fetchData from './scripts/fetchData';
 import { generateNewChart } from './scripts/createChart';
 
 document.addEventListener("DOMContentLoaded", function() {
-    console.log("The DOM has loaded");
-    fetchData();
-    generateNewChart();
-});
+    let currentTicker = '';
+    const tickerElement = document.getElementById('chart-container-ticker');
+    const randomSNP500 = ["MMM","ROKU","GOOGL","META","KO","GE"]
+    const intervalButtons = document.querySelectorAll('#date-intervals button');
 
-console.log("Hello World");
+});
