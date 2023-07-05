2611e04d71397b37717c795da336f80d1c9a50f7
Jordy <jordycorporan95@gmail.com>
2023-07-05 04:22:30 -0400

    Added async function inside event listener

--- src/index.js
+++ src/index.js
@@ -5,6 +5,19 @@ document.addEventListener("DOMContentLoaded", function() {
     let currentTicker = '';
     const tickerElement = document.getElementById('chart-container-ticker');
     const randomSNP500 = ["MMM","ROKU","GOOGL","META","KO","GE"]
+    //Selects all the buttons under id=date-intervals
     const intervalButtons = document.querySelectorAll('#date-intervals button');
+
+    async function updateDataForTicker(ticker,interval = '1month') {
+        currentTicker = ticker;
+        tickerElement.textContent = currentTicker.toUpperCase();
+        try {
+            const data = await fetchData(ticker,interval);
+            generateNewChart(data);
+        }catch (error) {
+            console.error(`Failed to get data for ticker ${ticker}:`, error)
+        }
+
+    }
 
 });
